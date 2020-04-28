cceb73b59c7b765a728a80a23da81506d86d15b9
Drew Weaver <usmcsoccer27@gmail.com>
2020-04-28 14:12:14 -0400

    setup server

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://drew_weaver_27:@Weaver27@cluster0-5hqst.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
